7e1d6cdcf9160d2cd2a602d844281de7ef4fe7f4
Barinov Maksim <craftyplonkton@gmail.com>
2023-11-27 22:43:37 +0300

    start project and add user model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
