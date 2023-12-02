01b494fb0c36d2e3ece79e5f13fba7ed44b246a2
Barinov Maksim <craftyplonkton@gmail.com>
2023-12-02 14:44:50 +0300

    add endpoints for Product and ProductDealer

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from products.views import DealerPriceViewSet
+from products.views import (DealerPriceViewSet, ProductDealerViewSet,
+                            ProductViewSet)
 
 v1_product_router = DefaultRouter()
-v1_product_router.register('products', DealerPriceViewSet, basename='products')
+v1_product_router.register(
+    'products', DealerPriceViewSet, basename='products')
+v1_product_router.register(
+    'own-products', ProductViewSet, basename='own_products')
+v1_product_router.register(
+    'matches', ProductDealerViewSet, basename='matches')
 
 urlpatterns = [
     path('', include(v1_product_router.urls))
